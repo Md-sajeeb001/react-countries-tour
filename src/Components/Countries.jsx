e9ee1256684ebb3eb57dd74e9a5b1077573d8acf
Md-sajeeb001 <mohammadsajeeb890@email.com>
2024-10-19 23:45:36 +0800

    added countrys list and dynamic style

--- src/Components/Countries.jsx
+++ src/Components/Countries.jsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from "react";
 import Country from "./country";
-import "./country.css"
+import "./country.css";
 const Countries = () => {
   const [countries, setCountries] = useState([]);
+  const [visitedCountries, setVisitedCountries] = useState([]);
 
   useEffect(() => {
     fetch("https://restcountries.com/v3.1/all")
@@ -10,12 +11,29 @@ const Countries = () => {
       .then((data) => setCountries(data));
   }, []);
 
+  const handelVisitedCountries = (country) => {
+    const newVisited = [...visitedCountries, country];
+    setVisitedCountries(newVisited);
+  };
+
   return (
     <div>
       <h3>Countris: {countries.length}</h3>
+      <div>
+        <h3>Mark Countries: {visitedCountries.length}</h3>
+        <ul>
+          {visitedCountries.map((country) => (
+            <li key={country.cca3}>{country.name.common}</li>
+          ))}
+        </ul>
+      </div>
       <div className="country-container">
         {countries.map((country) => (
-          <Country key={country.cca3} country={country}></Country>
+          <Country
+            handelVisitedCountries={handelVisitedCountries}
+            key={country.cca3}
+            country={country}
+          ></Country>
         ))}
       </div>
     </div>
--- src/Components/Country.jsx
+++ src/Components/Country.jsx
@@ -1,25 +1,27 @@
 import { useState } from "react";
 import "./country.css";
-const Country = ({ country }) => {
-  console.log(country);
+const Country = ({ country , handelVisitedCountries}) => {
   const { name, flags, flag, region, ccn3 } = country;
 
   const [visited, setVisited] = useState(false);
 
   const handelBtn = () => {
     setVisited(!visited);
-    
   };
 
+
+
   return (
-    <div className="country">
+    <div className={`country ${visited ? "visited" : "non-visited"}`}>
       <h4>Name: {name.common}</h4>
       <img src={flags.png} alt="" />
       <p>Flag: {flag}</p>
       <p>languages: {region}</p>
       <p>CCA3: {ccn3}</p>
+      <br />
+      <button onClick={()=>handelVisitedCountries(country)} className="btn">Mark Countries</button>
       <button id="button" onClick={handelBtn} className="btn">
-        {visited? "visited" : "visit"}
+        {visited ? "visited" : "visit"}
       </button>
       {visited ? "visited" : "not visited"}
     </div>
